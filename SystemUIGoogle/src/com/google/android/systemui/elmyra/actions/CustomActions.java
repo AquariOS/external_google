DirtyUnicorns/android_external_google

Active Edge: Restore the rest of the custom actions
Change-Id: Ifa82295857af5f6a1ab5acbc21f6889558aa1b97

SystemUIGoogle/src/com/google/android/systemui/elmyra/actions/CustomActions.java
@@ -83,19 +83,19 @@ public class CustomActions extends Action {
                 break;
             case 9: // Notification panel
                 if (isScreenOn) {
-                    // ActionUtils.toggleNotifications();
+                    ActionUtils.toggleNotifications();
                 }
                 break;
             case 10: // QS panel
                 if (isScreenOn) {
-                    // ActionUtils.toggleQsPanel();
+                    ActionUtils.toggleQsPanel();
                 }
                 break;
             case 11: // Clear notifications
-                // ActionUtils.clearAllNotifications();
+                ActionUtils.clearAllNotifications();
                 break;
             case 12: // Ringer modes
-                // ActionUtils.toggleRingerModes(getContext());
+                ActionUtils.toggleRingerModes(getContext());
                 break;
         }
     }

AquariOS/external_google

SystemUIGoogle/src/com/google/android/systemui/elmyra/actions/CustomActions.java
@@ -83,19 +83,19 @@ public class CustomActions extends Action {
                 break;
             case 9: // Notification panel
                 if (isScreenOn) {
-                   // AquaUtils.toggleNotifications();
+                   AquaUtils.toggleNotifications();
                 }
                 break;
             case 10: // QS panel
                 if (isScreenOn) {
-                    // AquaUtils.toggleQsPanel();
+                    AquaUtils.toggleQsPanel();
                 }
                 break;
             case 11: // Clear notifications
-                // AquaUtils.clearAllNotifications();
+                AquaUtils.clearAllNotifications();
                 break;
             case 12: // Ringer modes
-                // AquaUtils.toggleRingerModes(getContext());
+                AquaUtils.toggleRingerModes(getContext());
                 break;
         }
     }